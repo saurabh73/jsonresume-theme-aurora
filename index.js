f58f82aa6e9b113a5706a0ef6bfb0a48afb34cbb
Saurabh Dutta <saurabhdutta.jk@gmail.com>
2021-06-21 01:05:45 +0100

    add logging

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 const HTMLInlineCSSWebpackPlugin = require("html-inline-css-webpack-plugin").default;
 const PATHS = {
-	src: Path.join(__dirname, "src"),
-	dest: Path.join(__dirname, "public")
+  src: Path.join(__dirname, "src"),
+  dest: Path.join(__dirname, "public")
 };
 
 
@@ -25,25 +25,35 @@ async function runWebpack(compiler) {
 
 // TODO: return index.html with inline template
 async function render(resume) {
-  console.log("Rendering with THEME ", process.env.THEME_NO || 0);
-  // extract and replace HtmlWebpackPlugin with provided resume
-  const plugins = config.plugins.filter(i => (((i instanceof HtmlWebpackPlugin) == false) && ((i instanceof ScriptExtHtmlWebpackPlugin) == false)) && ((i instanceof HTMLInlineCSSWebpackPlugin) == false));
-  plugins.push(
-    new HtmlWebpackPlugin({
-      template: "./src/resume.hbs", 
-      filename: "index.html",
-      templateParameters: resume
-    }),
-    new ScriptExtHtmlWebpackPlugin({
-      inline: [/\.js$/],
-    }),
-    new HTMLInlineCSSWebpackPlugin()
-  );
-  config.plugins = plugins;
-  const compiler = webpack(config);
-  await runWebpack(compiler);
-  const filepath = Path.join(PATHS.dest, "index.html")
-  return fs.readFileSync(filepath, "utf-8");
+  try {
+
+
+    console.log("Rendering with THEME ", process.env.THEME_NO || 0);
+    // extract and replace HtmlWebpackPlugin with provided resume
+    const plugins = config.plugins.filter(i => (((i instanceof HtmlWebpackPlugin) == false) && ((i instanceof ScriptExtHtmlWebpackPlugin) == false)) && ((i instanceof HTMLInlineCSSWebpackPlugin) == false));
+    plugins.push(
+      new HtmlWebpackPlugin({
+        template: "./src/resume.hbs",
+        filename: "index.html",
+        templateParameters: resume
+      }),
+      new ScriptExtHtmlWebpackPlugin({
+        inline: [/\.js$/],
+      }),
+      new HTMLInlineCSSWebpackPlugin()
+    );
+    config.plugins = plugins;
+    console.log(JSON.stringify(config, null, 2));
+    console.log("Create compiler")
+    const compiler = webpack(config);
+    console.log("run runWebpack")
+    await runWebpack(compiler);
+    console.log("end runWebpack")
+    const filepath = Path.join(PATHS.dest, "index.html")
+    return fs.readFileSync(filepath, "utf-8");
+  } catch (err) {
+    console.error(err)
+  }
 }
 module.exports = {
   render: render
